12fd330fc629af3571d9a2f11f8dd33aebd454fc
David-Silva <orpi4488@gmail.com>
2025-09-24 19:29:02 -0500

    feet : create main menu

--- Program.cs
+++ Program.cs
@@ -9,3 +9,41 @@ var Patient1 = new Patient
 
 };
 
+
+static void MainMenu()
+{
+    while (true)
+    {
+        Console.WriteLine("----Welcome your Manager---");
+        Console.WriteLine("Choose the corresponding option");
+        Console.WriteLine(" [1] Register patient");
+        Console.WriteLine(" [2] Patient List");
+        Console.WriteLine(" [3] Search Patient");
+        Console.WriteLine(" [4] Exit");
+
+        string respuesta = Console.ReadLine() ?? "";
+
+        switch (respuesta)
+        {
+            case "1":
+                Console.WriteLine("aqui se agrega un paciente");
+                break;
+            case "2":
+                Console.WriteLine("aqui se ve la lista de pacientes");
+                break;
+            case "3":
+                Console.WriteLine("aqui se buscan los pacientes ");
+                break;
+            case "4":
+                Console.WriteLine("aqui se sale");
+                break;
+            default:
+                Console.WriteLine("opcion erronea");
+                break;
+        }
+    }
+
+
+}
+
+MainMenu();
