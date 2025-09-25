1d2fd4207190144a3fce1855e5f77090aa21fb77
David-Silva <orpi4488@gmail.com>
2025-09-24 19:09:08 -0500

    feet : create class patient

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using health_clinic.models;
+
+var Patient1 = new Patient
+{
+    Id = 1,
+    Name = "pedro",
+    Age = 5,
+    Symptoms = "moquillo"
+
+};
+
---
+++ models/Patient.cs
@@ -0,0 +1,14 @@
+
+
+namespace health_clinic.models;
+
+public class Patient
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+    
+    public string Symptoms { get; set; }
+ }
